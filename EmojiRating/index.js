4b21d9b8c9409678b935b6cfd483519a94ed624f
Aman <amanupadhyay404@gmail.com>
2022-09-30 11:13:01 +0530

    30-9-2022:11-12:Friday

---
+++ EmojiRating/index.js
@@ -0,0 +1,28 @@
+const star = document.querySelectorAll(".fa-star");
+const emojis=document.querySelectorAll(".far");
+const colors=["red","orange","lightblue","lightgreen","green"]
+
+
+upadate(0)
+// console.log(star)
+star.forEach((ele, index) => {
+    ele.addEventListener("click", () => {
+        // console.log("clicked",ind)
+        upadate(index)
+    })
+})
+
+function upadate(index) {
+    star.forEach((el, id) => {
+        if (id < index + 1) {
+            el.classList.add("active");
+        }else{
+            el.classList.remove("active");
+        }
+    })
+
+    emojis.forEach((ele,ind)=>{
+        ele.style.transform=`translateX(-${index*50}px)`;
+        ele.style.color=colors[index];
+    })
+}
